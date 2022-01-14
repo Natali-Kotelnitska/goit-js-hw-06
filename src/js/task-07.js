2036d00b2b64d4db5419a6e82e3c7638f58eb3b2
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-01-14 02:10:38 +0200

    updates task 8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,13 +3,8 @@
 // изменяет инлайн - стиль span#text обновляя свойство font - size.
 // В результате при перетаскивании ползунка будет меняться размер текста.
 
-/* <input id="font-size-control" type="range" min="16" max="96" />
-<br />
-<span id="text">Abracadabra!</span> */
-
 const rangeControlEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
-// console.log(textEl)
 
 rangeControlEl.addEventListener('change', onTextSizeChange);
 
