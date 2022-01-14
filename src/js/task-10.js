1a5785897fc21575008073dc8066375620d9116d
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-01-14 16:00:23 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,71 @@
+// Напиши скрипт создания и очистки коллекции элементов.
+// Пользователь вводит количество элементов в input и нажимает кнопку Создать,
+// после чего рендерится коллекция.
+// При нажатии на кнопку Очистить, коллекция элементов очищается.
+
+{
+  /* <div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+
+<div id="boxes"></div> */
+}
+
+// Создай функцию createBoxes(amount), которая принимает
+// один параметр - число.Функция создает столько < div >,
+// сколько указано в amount и добавляет их в div#boxes.
+
+// Размеры самого первого <div> - 30px на 30px.
+// Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
+// Все элементы должены иметь случайный цвет фона в формате HEX.
+// Используй готовую функцию getRandomHexColor для получения цвета.
+
+// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+
+let amount = 0;
+const inputEl = document.querySelector('input');
+const buttonCreateEl = document.querySelector('button[data-create]');
+const buttonDestroyEl = document.querySelector('button[data-destroy]');
+const boxesContainerEl = document.getElementById('boxes');
+//
+
+inputEl.addEventListener('input', onOutputChange);
+buttonCreateEl.addEventListener('click', createBoxes);
+buttonDestroyEl.addEventListener('click', destroyBoxes);
+
+let boxSize = 30;
+
+function onOutputChange(event) {
+  amount = event.currentTarget.value;
+  console.log(amount);
+  createBoxes(amount);
+}
+
+function createBoxes(amount) {
+  console.log('click');
+  for (let i = 0; i <= amount; i++) {
+    boxSize += 10;
+    const box = document.createElement('div');
+    box.style.width = boxSize + 'px';
+    box.style.height = boxSize + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = '2px';
+    box.style.border = 'solid 1px black';
+
+    boxesContainerEl.append(box);
+    return box;
+  }
+}
+
+// функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
+function destroyBoxes() {
+  boxesContainerEl.innerHTML = '';
+  boxSize = 30;
+}
+
+// Рандомна Функція зміни кольру для div
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
