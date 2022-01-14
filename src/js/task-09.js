a4dc577315db00ba409a72662ca75dc046f3c2ef
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-01-14 11:13:40 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -14,6 +14,16 @@
 }
 
 // Для генерации случайного цвета используй функцию getRandomHexColor.
+const body = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanColorEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onBodyColorChange);
+
+function onBodyColorChange(evt) {
+  spanColorEl.textContent = body.style.backgroundColor = getRandomHexColor();
+  // spanColorEl.textContent = getRandomHexColor();
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
